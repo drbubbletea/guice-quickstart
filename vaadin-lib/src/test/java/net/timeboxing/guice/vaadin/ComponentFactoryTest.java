a5d339a14ff2689c6046e22f65773b5e0f567c76
Chris Langewisch <chris.langewisch@gmail.com>
2022-08-05 23:57:53 -0400

    Thinking about ComponentFactory tests.

---
+++ vaadin-lib/src/test/java/net/timeboxing/guice/vaadin/ComponentFactoryTest.java
@@ -0,0 +1,27 @@
+package net.timeboxing.guice.vaadin;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ComponentFactoryTest {
+
+    @Test
+    public void supportsExactClass() {
+        Assertions.fail();
+    }
+
+    @Test
+    public void supportsChildClass() {
+        Assertions.fail();
+    }
+
+    @Test
+    public void supportsInterface() {
+        Assertions.fail();
+    }
+
+    @Test
+    public void canCreate() {
+        Assertions.fail();
+    }
+}
