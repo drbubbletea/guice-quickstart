b86575ec4201d7fc52a7bf6268a3b641ea558246
Chris Langewisch <chris.langewisch@gmail.com>
2022-08-05 21:34:57 -0400

    Working on ComponentFactories.

--- vaadin-lib/src/test/java/net/timeboxing/guice/vaadin/ComponentFactoriesTest.java
+++ vaadin-lib/src/test/java/net/timeboxing/guice/vaadin/ComponentFactoriesTest.java
@@ -1,12 +1,26 @@
 package net.timeboxing.guice.vaadin;
 
+import com.vaadin.flow.component.Component;
+import net.timeboxing.vaadin.component.ComponentFactories;
+import net.timeboxing.vaadin.component.ComponentFactory;
+import net.timeboxing.vaadin.component.ComponentPurpose;
+import net.timeboxing.vaadin.component.DefaultComponentFactories;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
 
 public class ComponentFactoriesTest {
 
     @Test
     public void noFactoriesReturnsEmpty() {
-        Assertions.fail();
+        Set<ComponentFactory<?>> factories = new HashSet<>();
+        ComponentFactories componentFactories = new DefaultComponentFactories(factories);
+
+        Optional<Component> result = componentFactories.get(new String(), ComponentPurpose.VIEW);
+
+        Assertions.assertTrue(result.isEmpty());
     }
 }
