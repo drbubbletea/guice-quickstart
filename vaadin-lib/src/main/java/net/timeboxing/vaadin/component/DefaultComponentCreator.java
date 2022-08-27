0d272ee93492c8074641f0b58815ce7ece0a3f71
Chris Langewisch <chris.langewisch@gmail.com>
2022-08-27 01:19:44 -0400

    Working on component creator.

--- vaadin-lib/src/main/java/net/timeboxing/vaadin/component/DefaultComponentCreator.java
+++ vaadin-lib/src/main/java/net/timeboxing/vaadin/component/DefaultComponentCreator.java
@@ -1,13 +1,22 @@
 package net.timeboxing.vaadin.component;
 
+import com.google.inject.Injector;
 import com.vaadin.flow.component.Component;
 
 public class DefaultComponentCreator implements ComponentCreator {
 
+    private final Injector injector;
 
+    public DefaultComponentCreator(Injector injector) {
+        this.injector = injector;
+    }
     @Override
     public Component create(Class<?> clazz, ComponentPurpose purpose) {
         try {
+            // get constructor
+            // create instance from constructor
+            // inject
+            // return
             return (Component) clazz.newInstance();
         } catch (Exception e) {
             throw new RuntimeException(e);
