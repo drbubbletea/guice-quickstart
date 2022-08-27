bfcb89d1e8f6c4d9a23a7802395aa271974f6d56
Chris Langewisch <chris.langewisch@gmail.com>
2022-08-27 01:25:19 -0400

    Can find constructor.

--- vaadin-lib/src/main/java/net/timeboxing/vaadin/component/DefaultComponentCreator.java
+++ vaadin-lib/src/main/java/net/timeboxing/vaadin/component/DefaultComponentCreator.java
@@ -3,17 +3,24 @@ package net.timeboxing.vaadin.component;
 import com.google.inject.Injector;
 import com.vaadin.flow.component.Component;
 
+import javax.inject.Inject;
+import java.lang.reflect.Constructor;
+
 public class DefaultComponentCreator implements ComponentCreator {
 
     private final Injector injector;
+    private final Class<?> componentClass;
 
-    public DefaultComponentCreator(Injector injector) {
+    public DefaultComponentCreator(Class<?> componentClass, Injector injector) {
+        this.componentClass = componentClass;
         this.injector = injector;
     }
     @Override
     public Component create(Class<?> clazz, ComponentPurpose purpose) {
         try {
-            // get constructor
+            // get constructor (TODO: do once per class in constructor)
+            Constructor<?> ctr = getConstructor(componentClass);
+
             // create instance from constructor
             // inject
             // return
@@ -21,5 +28,14 @@ public class DefaultComponentCreator implements ComponentCreator {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private Constructor<?> getConstructor(Class<?> clazz) {
+        for (Constructor<?> constructor: clazz.getConstructors()) {
+            if (constructor.isAnnotationPresent(Inject.class)) {
+                return constructor;
+            }
+        }
+        throw new ComponentAdapterException(String.format("No constructor annotated with javax.inject.Inject found in %s", clazz.getName()));
     }
 }
