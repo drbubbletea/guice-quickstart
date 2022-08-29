a6d7b8f507a72768c289cb5e1ef0f97c15a1b473
Chris Langewisch <chris.langewisch@gmail.com>
2022-08-28 22:00:11 -0400

    Fix module scope parameter.

--- vaadin-lib/src/main/java/net/timeboxing/vaadin/guice/VaadinComponentEventModule.java
+++ vaadin-lib/src/main/java/net/timeboxing/vaadin/guice/VaadinComponentEventModule.java
@@ -1,24 +1,26 @@
 package net.timeboxing.vaadin.guice;
 
 import com.google.inject.AbstractModule;
-import com.google.inject.Scope;
 import net.timeboxing.vaadin.event.VaadinComponentEventBus;
+import net.timeboxing.vaadin.event.impl.DefaultVaadinComponentEventBus;
+
+import java.lang.annotation.Annotation;
 
 public class VaadinComponentEventModule extends AbstractModule {
 
-    private final Scope scope;
+    private final Class<? extends Annotation> scope;
 
     /**
      *
      * @param scope
      */
-    public VaadinComponentEventModule(Scope scope) {
+    public VaadinComponentEventModule(Class<? extends Annotation> scope) {
         this.scope = scope;
     }
 
     @Override
     protected void configure() {
         super.configure();
-        bind(VaadinComponentEventBus.class).in(scope);
+        bind(VaadinComponentEventBus.class).to(DefaultVaadinComponentEventBus.class).in(scope);
     }
 }
