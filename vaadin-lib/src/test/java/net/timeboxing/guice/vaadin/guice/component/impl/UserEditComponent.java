da6766f8d9c4fcb7501020871b8211566fcd477d
Chris Langewisch <chris.langewisch@gmail.com>
2023-09-04 09:37:12 -0400

    Various changes. WIP.

--- vaadin-lib/src/test/java/net/timeboxing/guice/vaadin/guice/component/impl/UserEditComponent.java
+++ vaadin-lib/src/test/java/net/timeboxing/guice/vaadin/guice/component/impl/UserEditComponent.java
@@ -2,15 +2,15 @@ package net.timeboxing.guice.vaadin.guice.component.impl;
 
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.html.Label;
-import net.timeboxing.vaadin.component.AbstractVaadinComponent;
 import net.timeboxing.vaadin.component.ComponentFor;
 import net.timeboxing.vaadin.component.ComponentPurpose;
 import net.timeboxing.vaadin.component.Source;
+import net.timeboxing.vaadin.component.VaadinComponent;
 
 import javax.inject.Inject;
 
 @ComponentFor(forClass = User.class, purpose = ComponentPurpose.EDIT)
-public class UserEditComponent extends AbstractVaadinComponent {
+public class UserEditComponent implements VaadinComponent {
 
     private final User user;
     private final Component component;
