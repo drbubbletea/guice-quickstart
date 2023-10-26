cb1f559b262b11ff99bb04a9fff5503b7725d58a
Chris Langewisch <chris.langewisch@gmail.com>
2023-10-26 02:43:55 -0400

    Additional modules.

---
+++ lib-adapter/src/main/java/net/timeboxing/adapter/guice/AdapterModule.java
@@ -0,0 +1,11 @@
+package net.timeboxing.adapter.guice;
+
+import com.google.inject.AbstractModule;
+
+public class AdapterModule extends AbstractModule {
+
+    @Override
+    protected void configure() {
+
+    }
+}
