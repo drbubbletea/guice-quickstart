e72cb0036bc35d0dbb45d88949963efd93ac0f46
Chris Langewisch <chris.langewisch@gmail.com>
2023-11-12 14:51:12 -0500

    Support custom annotation which we can use for Vaadin ComponentFor annotation later...

--- lib-adapter/src/main/java/net/timeboxing/adapter/AdaptedFrom.java
+++ lib-adapter/src/main/java/net/timeboxing/adapter/AdaptedFrom.java
@@ -5,6 +5,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Annotate classes which are adapter targets.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface AdaptedFrom {
--- lib-adapter/src/test/java/net/timeboxing/adapter/GuiceAOPAdapterTest.java
+++ lib-adapter/src/test/java/net/timeboxing/adapter/GuiceAOPAdapterTest.java
@@ -4,10 +4,12 @@ import com.google.inject.Guice;
 import com.google.inject.Injector;
 import net.timeboxing.adapter.impl.CustomPurpose;
 import net.timeboxing.adapter.impl.DomainFactory;
+import net.timeboxing.adapter.impl.FooBar;
 import net.timeboxing.adapter.impl.TestAdapterModule;
 import net.timeboxing.adapter.impl.User;
 import net.timeboxing.adapter.impl.widget.CustomFooUserWidget;
 import net.timeboxing.adapter.impl.widget.DefaultPurposeUserWidget;
+import net.timeboxing.adapter.impl.widget.FooBarFooUserWidget;
 import net.timeboxing.adapter.impl.widget.Widget;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -45,5 +47,15 @@ class GuiceAOPAdapterTest {
         Assertions.assertTrue(userWidget.get().getClass().isAssignableFrom(CustomFooUserWidget.class));
         Widget widget = userWidget.get();
         Assertions.assertEquals("Crazy Bob foo", widget.display());
+    }
+    @Test
+    void canAdaptCustomAnnotationCustomPurpose() {
+        Injector injector = Guice.createInjector(new TestAdapterModule("net.timeboxing.adapter"));
+        DomainFactory factory = injector.getInstance(DomainFactory.class);
+        User user = factory.create("Crazy Bob");
+        Optional<Widget> userWidget = user.adaptTo(Widget.class, FooBar.class, FooBar.FOO);
+        Assertions.assertTrue(userWidget.get().getClass().isAssignableFrom(FooBarFooUserWidget.class));
+        Widget widget = userWidget.get();
+        Assertions.assertEquals("Crazy Bob FooBar default foo", widget.display());
     }
 }
