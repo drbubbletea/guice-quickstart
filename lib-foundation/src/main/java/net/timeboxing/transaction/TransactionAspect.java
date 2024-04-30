596791b8ab4a5319dee291bc093c13568d85e115
Chris Langewisch <chris.langewisch@gmail.com>
2024-04-30 00:23:48 -0400

    Ready for testinng

--- lib-foundation/src/main/java/net/timeboxing/transaction/TransactionAspect.java
+++ lib-foundation/src/main/java/net/timeboxing/transaction/TransactionAspect.java
@@ -7,9 +7,13 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.reflect.MethodSignature;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Aspect
 public class TransactionAspect {
+
+    private static final Logger LOG = LoggerFactory.getLogger(TransactionAspect.class);
 
     @Inject
     private TransactionManager transactionManager;
@@ -17,27 +21,41 @@ public class TransactionAspect {
     @Around("@annotation(net.timeboxing.transaction.Transactional)")
     public Object wrapTransaction(ProceedingJoinPoint pjp) throws Exception {
         Transactional annotation = ((MethodSignature) pjp.getSignature()).getMethod().getAnnotation(Transactional.class);
+        boolean doRollback = false;
         try {
-            transactionManager.begin();
+            if (0 == transactionManager.getStatus()) {
+                LOG.trace("Existing transaction found");
+            } else {
+                LOG.trace("Starting transaction");
+                transactionManager.begin();
+            }
             return pjp.proceed(pjp.getArgs());
+        } catch (Exception e) {
+            doRollback = shouldRollback(annotation, e);
+            throw e;
         } catch (Throwable e) {
-            rollbackOrCommit(annotation, e);
+            doRollback = true;
+            throw new TransactionException("Unexpected Throwable", e);
         } finally {
-            transactionManager.commit();
+            if (!doRollback) {
+                transactionManager.commit();
+            } else {
+                transactionManager.rollback();
+            }
         }
     }
 
-    private void rollbackOrCommit(Transactional annotation, Throwable e) throws Exception {
+    private boolean shouldRollback(Transactional annotation, Throwable e) {
         for (Class<?> noRollback: annotation.dontRollbackOn()) {
             if (noRollback.isAssignableFrom(e.getClass())) {
-                transactionManager.commit();
-                return;
+                LOG.trace("No rollback for exception '{}'", e.getClass().getCanonicalName());
+                return false;
             }
         }
         for (Class<?> doRollback: annotation.rollbackOn()) {
             if (doRollback.isAssignableFrom(e.getClass())) {
-                transactionManager.rollback();
-                return;
+                LOG.trace("Rollback for exception '{}'", e.getClass().getCanonicalName());
+                return true;
             }
         }
     }
--- lib-foundation/src/main/java/net/timeboxing/transaction/TransactionException.java
+++ lib-foundation/src/main/java/net/timeboxing/transaction/TransactionException.java
@@ -3,4 +3,23 @@ package net.timeboxing.transaction;
 import net.timeboxing.exception.ApplicationException;
 
 public class TransactionException extends ApplicationException {
+    public TransactionException() {
+        super();
+    }
+
+    public TransactionException(String message) {
+        super(message);
+    }
+
+    public TransactionException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public TransactionException(Throwable cause) {
+        super(cause);
+    }
+
+    protected TransactionException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
 }
